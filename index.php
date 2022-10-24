0992163ae4835e775f4b134ed436de5a1a07841d
elsanadila <febiaelsanadila12@gmail.com>
2022-10-24 11:03:35 +0700

    belajar php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello Elsa <br>";
+
+$nama="Nadila";
+$umur= 21;
+
+echo "Halo nama saya <strong>$nama</strong>, berumur $umur tahun. <br>";
+
+$namadua="Anna";
+$umurdua= 15;
+
+echo "Saya mempunyai adik bernama <strong>$namadua</strong>, yang berumur $umurdua tahun.<br>";
+
+$selisihumur= $umur-$umurdua;
+
+echo "Selisih umur yang terpaut dengan adik saya adalah $selisihumur tahun."
+
+?>
